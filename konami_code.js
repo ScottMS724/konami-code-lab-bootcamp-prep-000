403819717b0bbd923a1542ed3131d3901452c900
Scott Silverman <scottms724@gmail.com>
2018-02-07 19:19:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  document.body.addEventListener('keydown', function(e)) {
-    
+  document.body.addEventListener('keydown', function(e) {
+    var key = parseInt(e.which || e.detail);
+    var index = 0
+  }
   }
 
 const alphabet = []

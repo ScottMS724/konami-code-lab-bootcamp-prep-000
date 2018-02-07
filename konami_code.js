6b8d2e70bc1e61b19bebac3cc2bea558c6ebcde3
Scott Silverman <scottms724@gmail.com>
2018-02-07 19:35:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
    document.body.addEventListener('keydown', function(e) {
-      var key = parseInt(e.which);
+      var key = parseInt(e.detail || e.which);
       var index=0;
       if(key === code[index]) {
       index++;

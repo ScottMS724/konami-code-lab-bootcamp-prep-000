933f80583fd93212669feb3cf67b2bb6f26caf1c
Scott Silverman <scottms724@gmail.com>
2018-02-07 17:50:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', event);
+}
 
+const alphabet = []
+let index = 0;
+function onKeyDownHandler(e) {
+  const key = parseInt(e.which);
+  if (key === alphabet[index]) {
+    index++;
+  if (index === alphabet.length) {
+    alert("Hurray!");
+    index = 0;
+  }
+  } else {
+    index = 0;
+  }
 }
